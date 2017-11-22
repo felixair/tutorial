14f80e99735c61aadb210db1386be3c261493d04
qin.kefu <qin.kefu@navercorp.com>
2017-11-22 15:23:31 +0800

    modified

--- tutorial/spiders/blog_spider.py
+++ tutorial/spiders/blog_spider.py
@@ -4,7 +4,7 @@ import scrapy
 from tutorial.items import DmozItem
 
 class BlogSpider(scrapy.Spider):
-    name = "blog"
+    name = 'blog'
     start_urls = [
         'https://blog.scrapinghub.com'
     ]
@@ -12,9 +12,11 @@ class BlogSpider(scrapy.Spider):
     def parse(self, response):
         for title in response.css('h2.entry-title'):
             item = DmozItem()
-            item['title'] = title.css('a ::text').extract_first()
+            item['title'] = title.css('a ::text').extract_first()   #extract_first()返回字符串 extract()返回数组
+            item['link'] = title.css('a ::attr("href")').extract_first()
             yield item
         
-        for next_page in response.css('div.prev-post > a'):
-            yield response.follow(next_page, self.parse)
+        #各个页面数据循环获取
+        # for next_page in response.css('div.prev-post > a'):
+        #     yield response.follow(next_page, self.parse)
 
